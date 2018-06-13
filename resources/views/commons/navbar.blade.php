979d471794f96fe65601664ac0e624f8ede1e9e8
yuina.asano <yuina.asano@rakuten.com>
2018-06-13 07:01:12 +0000

    kadai1

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,4 +1,3 @@
-
 <header>
     <nav class="navbar navbar-inverse navbar-static-top">
         <div class="container">
@@ -13,7 +12,20 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('tasks.create', 'Create new task') !!}</li>
+                    @if (Auth::check())
+                        <li>{!! link_to_route('users.index', 'Users') !!}</li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li>{!! link_to_route('users.show', 'My profile', ['id' => Auth::id()]) !!}</li>
+                                <li role="separator" class="divider"></li>
+                                <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            </ul>
+                        </li>
+                    @else
+                        <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                        <li>{!! link_to_route('login', 'Login') !!}</li>
+                    @endif
                 </ul>
             </div>
         </div>
