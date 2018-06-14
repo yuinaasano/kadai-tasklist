a7c0f8e1f884a1cb3f857ec75451a6b3ef4dadac
yuina.asano <yuina.asano@rakuten.com>
2018-06-14 06:36:36 +0000

    kadai

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -22,6 +22,7 @@
                   {!! Form::open(['route' => 'tasks.store']) !!}
                       <div class="form-group">
                           {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
+                           {!! Form::textarea('status', old('status'), ['class' => 'form-control', 'rows' => '2']) !!}
                           {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
                       </div>
                   {!! Form::close() !!}
